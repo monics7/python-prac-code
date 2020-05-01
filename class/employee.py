b34df44216b096dbea4e6b4168544b5edf764403
Monica Maibram <maibram.monica06@gmail.com>
2020-05-01 14:23:08 +0530

    updated employee code

--- class/employee.py
+++ class/employee.py
@@ -4,15 +4,15 @@ class Employee:
     amount_raise = 1.04
 
     def __init__(self, first, last, pay):
-        self.fname = first
-        self.lname = last
-        self.salary = pay
+        self.first = first
+        self.last = last
+        self.pay = pay
         self.email = first+'.'+last+'@company.com'
 
         Employee.num_of_emps += 1
 
     def full_name(self):
-        return '{} {}'.format(self.fname, self.lname)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.salary = int(self.salary*self.amount_raise)
